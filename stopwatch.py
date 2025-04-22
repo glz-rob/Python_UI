a906f777b75f3d0ee4cf2eb6b59b37c479fc0f69
Roberto González <roberto.gonzalez@sparktechs.com>
2025-04-22 10:44:11 -0700

    Add styles to widgets

--- stopwatch.py
+++ stopwatch.py
@@ -1,22 +1,48 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.widgets import Button, Digits, Footer, Header
+
+
+class TimeDisplay(Digits):
+    """Displays elapsed time"""
+
+
+class Stopwatch(HorizontalGroup):
+    """Stopwatch widget"""
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Called when a button is pressed"""
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of stopwatch"""
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+        yield TimeDisplay("00:00:00.00")
 
 
 class StopwatchApp(App):
     """Stopwatch manager"""
 
+    CSS_PATH = "stopwatch03.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app"""
         yield Header()
         yield Footer()
+        yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch())
 
-    def action_toggle_dark(self):
+    def action_toggle_dark(self) -> None:
         """Toggles the dark mode"""
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
+
 
 if __name__ == "__main__":
     app = StopwatchApp()
